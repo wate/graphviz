263a5e6b327442eb9ccd2d82fef922bcf3e618ee
wate <pc.poisoning@gmail.com>
2018-12-27 02:29:05 +0900

    replaced alias_method_chain with prepend

--- lib/graphviz_helper_patch.rb
+++ lib/graphviz_helper_patch.rb
@@ -2,11 +2,10 @@ require_dependency 'redmine/wiki_formatting/markdown/helper'
 
 module GraphvizHelperPatch
   def self.included(base) # :nodoc:
-    base.send(:include, HelperMethodsWikiExtensions)
+    base.send(:prepend, HelperMethodsWikiExtensions)
 
     base.class_eval do
       unloadable # Send unloadable so it will not be unloaded in development
-      alias_method_chain :heads_for_wiki_formatter, :graphviz
     end
   end
 end
@@ -14,8 +13,9 @@ end
 module HelperMethodsWikiExtensions
   # extend the editor Toolbar for adding a graphviz button
   # overwrite this helper method to have full control about the load order
-  def heads_for_wiki_formatter_with_graphviz
+  def heads_for_wiki_formatter
     return if @heads_for_wiki_graphviz_included
+    super
     content_for :header_tags do
       javascript_include_tag('jstoolbar/jstoolbar-textile.min') +
         javascript_include_tag("jstoolbar/lang/jstoolbar-#{current_language.to_s.downcase}") +
